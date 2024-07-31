43fced901b4290088a964a412c40cef6b6a5a374
kishorec11 <kishorekumar@dckap.com>
2024-07-31 13:07:08 +0530

    file added

--- Ecommercesdk/src/index.tsx
+++ Ecommercesdk/src/index.tsx
@@ -2,11 +2,12 @@ export function multiply(a: number, b: number): Promise<number> {
   return Promise.resolve(a * b);
 }
 
+
 import * as FileSystem from 'expo-file-system';
 import { ApolloClient, HttpLink, InMemoryCache, gql } from '@apollo/client';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-
 class MagentoGraphQL {
+
   static _url = '';
   static _accessToken = '';
   static _cacheStore = null;
@@ -43,120 +44,122 @@ class MagentoGraphQL {
   }
 
   static async generate() {
+
+var IntrospectionQuery = gql`
+query IntrospectionQuery {
+  __schema {
+    queryType {
+      name
+    }
+    mutationType {
+      name
+    }
+    subscriptionType {
+      name
+    }
+    types {
+      ...FullType
+    }
+    directives {
+      name
+      description
+      locations
+      args {
+        ...InputValue
+      }
+    }
+  }
+}
+
+fragment FullType on __Type {
+  kind
+  name
+  description
+  fields(includeDeprecated: true) {
+    name
+    description
+    args {
+      ...InputValue
+    }
+    type {
+      ...TypeRef
+      fields(includeDeprecated: true) {
+        name
+        description
+        args {
+          ...InputValue
+        }
+        type {
+          ...TypeRef
+        }
+        isDeprecated
+        deprecationReason
+      }
+      inputFields {
+        ...InputValue
+      }
+      interfaces {
+        ...TypeRef
+      }
+      enumValues(includeDeprecated: true) {
+        name
+        description
+        isDeprecated
+        deprecationReason
+      }
+      possibleTypes {
+        ...TypeRef
+      }
+    }
+    isDeprecated
+    deprecationReason
+  }
+  inputFields {
+    ...InputValue
+  }
+  interfaces {
+    ...TypeRef
+  }
+  enumValues(includeDeprecated: true) {
+    name
+    description
+    isDeprecated
+    deprecationReason
+  }
+  possibleTypes {
+    ...TypeRef
+  }
+}
+
+fragment InputValue on __InputValue {
+  name
+  description
+  type {
+    ...TypeRef
+  }
+  defaultValue
+}
+
+fragment TypeRef on __Type {
+  kind
+  name
+  ofType {
+    kind
+    name
+    ofType {
+      kind
+      name
+      ofType {
+        kind
+        name
+      }
+    }
+  }
+}
+`;
     try {
       const result = await MagentoGraphQL.client().query({
-        query: gql(`
-          query IntrospectionQuery {
-            __schema {
-              queryType {
-                name
-              }
-              mutationType {
-                name
-              }
-              subscriptionType {
-                name
-              }
-              types {
-                ...FullType
-              }
-              directives {
-                name
-                description
-                locations
-                args {
-                  ...InputValue
-                }
-              }
-            }
-          }
-
-          fragment FullType on __Type {
-            kind
-            name
-            description
-            fields(includeDeprecated: true) {
-              name
-              description
-              args {
-                ...InputValue
-              }
-              type {
-                ...TypeRef
-                fields(includeDeprecated: true) {
-                  name
-                  description
-                  args {
-                    ...InputValue
-                  }
-                  type {
-                    ...TypeRef
-                  }
-                  isDeprecated
-                  deprecationReason
-                }
-                inputFields {
-                  ...InputValue
-                }
-                interfaces {
-                  ...TypeRef
-                }
-                enumValues(includeDeprecated: true) {
-                  name
-                  description
-                  isDeprecated
-                  deprecationReason
-                }
-                possibleTypes {
-                  ...TypeRef
-                }
-              }
-              isDeprecated
-              deprecationReason
-            }
-            inputFields {
-              ...InputValue
-            }
-            interfaces {
-              ...TypeRef
-            }
-            enumValues(includeDeprecated: true) {
-              name
-              description
-              isDeprecated
-              deprecationReason
-            }
-            possibleTypes {
-              ...TypeRef
-            }
-          }
-
-          fragment InputValue on __InputValue {
-            name
-            description
-            type {
-              ...TypeRef
-            }
-            defaultValue
-          }
-
-          fragment TypeRef on __Type {
-            kind
-            name
-            ofType {
-              kind
-              name
-              ofType {
-                kind
-                name
-                ofType {
-                  kind
-                  name
-                }
-              }
-            }
-          }
-        `),
+        query: IntrospectionQuery,
       });
 
       if (result.errors) {
@@ -165,10 +168,8 @@ class MagentoGraphQL {
       } else {
         const introspectionJson = result.data;
         console.log('====================================');
-        // console.log(introspectionJson);
-
-        // Store the introspection JSON in a file using Expo FileSystem
-        const path = `/Users/rhino/Downloads/Reactnative/mobile_sdk_package/Ecommercesdk/src/generated/introspection_result/introspection_result.json`;
+        // Define the path using Expo's FileSystem
+        const path = FileSystem.documentDirectory + 'introspection_result.json';
         await FileSystem.writeAsStringAsync(path, JSON.stringify(introspectionJson, null, 2));
         console.log(`Introspection JSON saved to ${path}`);
 
